4f0d9221bd9becbd5a21c09e2a4a7f2b826280ec
Garv <garvoberoi1999@gmail.com>
2020-11-07 13:43:11 +0530

    9

--- easyappointment/settings.py
+++ easyappointment/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'easyappointment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '<database_name>',
-        'USER': '<user_name>',
-        'PASSWORD': '<password>',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
